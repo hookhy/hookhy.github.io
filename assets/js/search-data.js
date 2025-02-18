e5d2ab282bf5970b4f560aa087eec6e62eee070e
hookhy <kweon186@hanyang.ac.kr>
2025-02-18 03:48:50 +0000

    Deploying to gh-pages from @ hookhy/hookhy.github.io@8c03b230afe42737073f4534b811a39a00a84753 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -439,8 +439,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-excited-to-start-my-postdoctoral-fellow-at-bch-hms-sunglasses-sparkles",
+          title: 'Excited to start my Postdoctoral Fellow at BCH/HMS!! :sunglasses: :sparkles:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
